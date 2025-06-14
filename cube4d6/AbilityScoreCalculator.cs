57e5d3253f8fb52c33f116ce36080e2b6690bd4e
Nikolay <nikolya.lysenko@mail.ru>
2025-06-14 23:25:29 +0500

    Add project files.

---
+++ cube4d6/AbilityScoreCalculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cube4d6
+{
+    class AbilityScoreCalculator
+    {
+        public int RollResult = 14; // результат броска
+        public double DivideBy = 1.75; // разделить на
+        public int AddAmount = 2; // добавить сумму
+        public int Minimum = 3;
+        public int Score; // счет
+
+        /// <summary>
+        /// Расчистывает сумму счета результата броска кубика
+        /// </summary>
+        public void CalculateAbilityScore()
+        {
+            // Результат броска делится на значение поля "разделить на"
+            double divide = RollResult / DivideBy;
+            // AddAmount прибавляется к результату деления
+            int added = AddAmount + (int)divide;
+
+            // Если результат слишком мал, используем минимальное значение
+            if (added < Minimum)
+            {
+                Score = Minimum;
+            }
+            else
+            {
+                Score = added;
+            }
+        }
+
+    }
+}
---
+++ cube4d6/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace cube4d6
+{
+    class Starting4D6Roll
+    {
+        public static void Main(string[] args)
+        {
+            AbilityScoreCalculator calculator = new AbilityScoreCalculator();
+            
+            while (true)
+            {
+                calculator.RollResult = ReadInt(calculator.RollResult, "Starting 4d6 roll");
+                calculator.DivideBy = ReadDouble(calculator.DivideBy, "Divide by");
+                calculator.AddAmount = ReadInt(calculator.AddAmount, "Add amount");
+                calculator.Minimum = ReadInt(calculator.Minimum, "Minimum");
+                calculator.CalculateAbilityScore();
+                Console.WriteLine("Calculated ability score: " + calculator.Score);
+                Console.WriteLine("Press Q to quit, any other key to continue");
+                char keyChar = Console.ReadKey(true).KeyChar;
+                if ((keyChar == 'Q') || (keyChar == 'q')) return;
+            }
+        }
+
+        /// <summary>
+        /// Выводит сообщение и читает значение int с консоли
+        /// </summary>
+        /// <param name="lastUserValue">Значение по умолчанию</param>
+        /// <param name="prompt">Сообщение, выводимое на консоль</param>
+        /// <returns>Прочитанное значение int или значение по умолчанию, если преобразование
+        /// невозможно.</returns>
+        public static int ReadInt(int lastUserValue, string prompt)
+        {
+            Console.Write(prompt + "[" + lastUserValue + "]");
+
+            if (int.TryParse(Console.ReadLine(), out int result))
+            {
+                Console.WriteLine("     using value " + result);
+                return result;
+                
+            }
+            return lastUserValue;
+        }
+        /// <summary>
+        /// Выводит сообщение и читает значение int с консоли
+        /// </summary>
+        /// <param name="lastUserValue">Значение по умолчанию</param>
+        /// <param name="prompt">Сообщение, выводимое на консоль</param>
+        /// <returns>Прочитанное значение int или значение по умолчанию, если преобразование
+        /// невозможно</returns>
+        public static double ReadDouble(double lastUserValue, string prompt)
+        {
+            Console.Write(prompt + "[" + lastUserValue + "]");
+            if (double.TryParse(Console.ReadLine(), out double result))
+            {
+                Console.WriteLine("     using value " + result);
+                return result;
+            }
+            return lastUserValue;
+        }
+    }
+}
